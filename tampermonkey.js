88a638bd107dda983ff2fb212bffd38840489770
Axel Andaroth <ax.fiolle@gmail.com>
2023-10-17 16:10:57 +0200

    get video dom once

--- tampermonkey.js
+++ tampermonkey.js
@@ -12,20 +12,19 @@
 (function() {
     'use strict';
     console.log('Youtube Anti-Adblock Killer by Axel Andaroth')
+    const video = document.querySelector('video')
     const interval = setInterval(() => { // lazy repeat (^:
         const dialogs = document.querySelectorAll('tp-yt-paper-dialog') // find all dialogs
         // find the anti-adblock one, use the expression you like:
         const antiAdBlockDialog = dialogs.find((d) => (!!d.innerHTML.toLowerCase().match(/block|publ|bloqu/g)))
         if (!!antiAdBlockDialog) {
             antiAdBlockDialog.style.display = "none" // hide the popup
-            const video = document.querySelector('video')
             if (!!video && video.paused) video.play() // auto play if was paused
             const close = antiAdBlockDialog.querySelector('div.yt-spec-touch-feedback-shape__fill') // find close button
             if (close) {
                 console.log('try close popup', close)
                 close.click() // press the close button
             } // endof close
-
         } // endof antiAdBlockDialog
     },1000) // endof interval
 })();
