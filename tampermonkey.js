66051c2ba954cfbfe54eeaea94379d1b1ba2ab8f
Axel Andaroth <ax.fiolle@gmail.com>
2023-10-17 09:31:00 +0200

    shorten + remove useless logs

--- tampermonkey.js
+++ tampermonkey.js
@@ -19,13 +19,11 @@
         if (killed) return;
         const popup = document.querySelector('ytd-popup-container') // find modal
         if (popup) {
-
             const dialog = document.querySelector('tp-yt-paper-dialog') // find dialog
             if (dialog) {
-                console.log('dialog found', dialog)
                 const close = document.querySelector('div.yt-spec-touch-feedback-shape__fill') // find close button
                 if (close) {
-                    console.log('close found', close)
+                    console.log('close popup', close)
                     close.click() // press the close button
                     killed = true;
                 }
