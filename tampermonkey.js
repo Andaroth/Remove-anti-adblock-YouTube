3051058ab13cd68787f3bccd647cca8ec73b5a90
Axel Andaroth <ax.fiolle@gmail.com>
2023-10-17 16:36:46 +0200

    more accurate dialog finder

--- tampermonkey.js
+++ tampermonkey.js
@@ -5,26 +5,31 @@
 // @description  Remove the anti-adblocker popup on Youtube
 // @author       Axel Andaroth
 // @match        https://www.youtube.com/*
-// @icon         data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw==
+// @icon         data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAYAAABzenr0AAAAAXNSR0IArs4c6QAAAC1JREFUWEft0EERAAAAAUH6lxbDZxU4s815PffjAAECBAgQIECAAAECBAgQIDAaPwAh6O5R/QAAAABJRU5ErkJggg==
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
     console.log('Youtube Anti-Adblock Killer by Axel Andaroth')
-    const video = document.querySelector('video')
+    const video = document.querySelector('video') // find the video player in page
+    let kill, click = false
     const interval = setInterval(() => { // lazy repeat (^:
+        if (kill) return console.log('master my work here is done');
         const dialogs = document.querySelectorAll('tp-yt-paper-dialog') || [] // find all dialogs
+        console.log('dialogs', dialogs)
         // find the anti-adblock one, use the expression you like:
-        const antiAdBlockDialog = Array.from(dialogs).find((d) => (!!d.innerHTML.toLowerCase().match(/block|publ|bloqu/g)))
+        const antiAdBlockDialog = Array.from(dialogs).find((d) => (!!d.innerHTML.toLowerCase().match(/allow youtube ads|autoriser youtube ads/g)))
         if (!!antiAdBlockDialog) {
+            console.log('antiAdBlockDialog', antiAdBlockDialog)
             antiAdBlockDialog.style.display = "none" // hide the popup
             if (!!video && video.paused) video.play() // auto play if was paused
-            const close = antiAdBlockDialog.querySelector('div.yt-spec-touch-feedback-shape__fill') // find close button
-            if (close) {
-                console.log('try close popup', close)
-                close.click() // press the close button
+            const closeBtn = antiAdBlockDialog.querySelector('div.yt-spec-touch-feedback-shape__fill') // find close button
+            if (closeBtn) {
+                console.log('try close popup', closeBtn)
+                closeBtn.click() // press the close button
+                click = true
             } // endof close
-        } // endof antiAdBlockDialog
+        } else if (click) kill = true; // if no dialog + closeBtn clicked, then its killed
     },1000) // endof interval
 })();
