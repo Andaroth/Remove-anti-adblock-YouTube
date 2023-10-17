10c5f83e86b9ba44e6979443b6518cc88f63c423
Axel Andaroth <ax.fiolle@gmail.com>
2023-10-17 15:02:05 +0200

    Shorter loop

--- tampermonkey.js
+++ tampermonkey.js
@@ -12,25 +12,19 @@
 (function() {
     'use strict';
     console.log('Youtube Anti-Adblock Killer by Axel Andaroth')
-
-    const interval = setInterval(() => {
-        const popup = document.querySelector('ytd-popup-container') // find modal
-        if (popup) {
-
-            const dialog = document.querySelector('tp-yt-paper-dialog') // find dialog
-            const isAntiAdBlockDialog = (dialog && (
-                !!dialog.innerHTML.toLowerCase().includes('blockers')
-                || !!dialog.innerHTML.toLowerCase().includes('bloqu')
-                || !!dialog.innerHTML.toLowerCase().includes('publ')
-            )) || false
-            if (isAntiAdBlockDialog) {
-                const close = dialog.querySelector('div.yt-spec-touch-feedback-shape__fill') // find close button
-                if (close) {
-                    console.log('close popup', close)
-                    close.click() // press the close button
-                }
-
-            }
-        }
-    },1000)
+    const interval = setInterval(() => { // lazy repeat (^:
+        const dialogs = document.querySelectorAll('tp-yt-paper-dialog') // find all dialogs
+        const antiAdBlockDialog = dialogs.find((d) => ( // find the anti-adblock one, use the expression you like:
+            !!d.innerHTML.toLowerCase().includes('block')
+            || !!d.innerHTML.toLowerCase().includes('bloqu')
+            || !!d.innerHTML.toLowerCase().includes('publ')
+        )) // endof find()
+        if (!!antiAdBlockDialog) {
+            const close = antiAdBlockDialog.querySelector('div.yt-spec-touch-feedback-shape__fill') // find close button
+            if (close) {
+                console.log('try close popup', close)
+                close.click() // press the close button
+            } // endof close
+        } // endof antiAdBlockDialog
+    },1000) // endof interval
 })();
