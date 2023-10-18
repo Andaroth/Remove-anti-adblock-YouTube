1f978cc0319a0be1a7fafff0e2069aa4dbaeaa65
Axel Andaroth <ax.fiolle@gmail.com>
2023-10-18 11:55:04 +0200

    Update tampermonkey.js

--- tampermonkey.js
+++ tampermonkey.js
@@ -13,13 +13,18 @@
     'use strict';
     console.log('Youtube Anti-Adblock Killer by Axel Andaroth')
     const video = document.querySelector('video') // find the video player in page
+    let overlay = null // DOM
     let closeBtnClicked = false
     const interval = setInterval(() => { // lazy repeat (^:
         const dialogs = document.querySelectorAll('tp-yt-paper-dialog') || [] // find all dialogs
+        if (Array.from(dialogs).length) console.log('dialogs opened:',dialogs)
         // find the anti-adblock one, use the expression you like:
-        const antiAdBlockDialog = Array.from(dialogs).find((d) => (!!d.innerHTML.toLowerCase().match(/allow youtube ads|autoriser youtube ads/g)))
+        const antiAdBlockDialog = Array.from(dialogs).find((d) => (!!d.innerHTML.toLowerCase().includes("bloqueur de publicité")))
         if (!!antiAdBlockDialog) { // there is an anti-adblock dialog
+            console.log('antiAdBlockDialog', antiAdBlockDialog)
             antiAdBlockDialog.style.display = "none" // hide the popup
+            if (!overlay) overlay = document.querySelector('tp-yt-iron-overlay-backdrop') // get overlay
+            else overlay.style.display = "none" // hide overlay
             if (!!video && video.paused) video.play() // auto play if was paused
             const closeBtn = antiAdBlockDialog.querySelector('div.yt-spec-touch-feedback-shape__fill') // find close button
             if (closeBtn) {
@@ -27,9 +32,6 @@
                 closeBtn.click() // press the close button
                 closeBtnClicked = true
             } // endof close
-        } else if (closeBtnClicked) {
-            console.log('MASTER I AM HERE TO SERVE YOU')
-            clearInterval(interval)
-        }; // if no dialog + closeBtnClicked, then its killed
+        } else if (closeBtnClicked) console.log('MASTER, I AM HERE TO SERVE YOU') // if no dialog + closeBtnClicked, then its killed
     },1000) // endof interval
 })();
