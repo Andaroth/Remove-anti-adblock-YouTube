fe67978018a93043dc7a80a858dde598651d753d
Axel Andaroth <ax.fiolle@gmail.com>
2023-10-19 09:18:51 +0200

    Update anti-adblocker v1

--- tampermonkey.js
+++ tampermonkey.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Youtube Anti-Adblock Killer
 // @namespace    https://anda.ninja/
-// @version      0.1
+// @version      1.0.0
 // @description  Remove the anti-adblocker popup on Youtube
 // @author       Axel Andaroth
 // @match        https://www.youtube.com/*
@@ -12,26 +12,21 @@
 (function() {
     'use strict';
     console.log('Youtube Anti-Adblock Killer by Axel Andaroth')
-    const video = document.querySelector('video') // find the video player in page
-    let overlay = null // DOM
-    let closeBtnClicked = false
+    let video = document.querySelector('video') // find the video player in page
+    let overlay, closeBtn = null // DOM
     const interval = setInterval(() => { // lazy repeat (^:
         const dialogs = document.querySelectorAll('tp-yt-paper-dialog') || [] // find all dialogs
         if (Array.from(dialogs).length) console.log('dialogs opened:',dialogs)
         // find the anti-adblock one, use the expression you like:
         const antiAdBlockDialog = Array.from(dialogs).find((d) => (!!d.innerHTML.toLowerCase().includes("bloqueur de publicité")))
         if (!!antiAdBlockDialog) { // there is an anti-adblock dialog
-            console.log('antiAdBlockDialog', antiAdBlockDialog)
             antiAdBlockDialog.style.display = "none" // hide the popup
             if (!overlay) overlay = document.querySelector('tp-yt-iron-overlay-backdrop') // get overlay
             else overlay.style.display = "none" // hide overlay
-            if (!!video && video.paused) video.play() // auto play if was paused
-            const closeBtn = antiAdBlockDialog.querySelector('div.yt-spec-touch-feedback-shape__fill') // find close button
-            if (closeBtn) {
-                console.log('try close popup with btn:', closeBtn)
-                closeBtn.click() // press the close button
-                closeBtnClicked = true
-            } // endof close
-        } else if (closeBtnClicked) console.log('MASTER, I AM HERE TO SERVE YOU') // if no dialog + closeBtnClicked, then its killed
+            if (!video) video = document.querySelector('video') // find the video player in page
+            video.play() // force play
+            if (!closeBtn) closeBtn = antiAdBlockDialog.querySelector('div.yt-spec-touch-feedback-shape__fill') // find close button
+            else closeBtn.click() // press the close button to prevent popup come back
+        }
     },1000) // endof interval
 })();
